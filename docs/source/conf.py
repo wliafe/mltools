3598c7e155723be307a4e58f99b205ee4de9c868
wliafe <914099435@qq.com>
2025-08-23 12:06:39 +0800

    更新docs配置

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,37 +6,44 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import tomli
+import sys
 from pathlib import Path
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[2]  # 项目根目录
 
+if sys.version_info >= (3, 11):
+    # Python 3.11 或更高
+    import tomllib as tomli
+else:
+    # Python 3.6 ~ 3.10
+    import tomli
+
 with open(ROOT / "pyproject.toml", "rb") as f:
     config = tomli.load(f)
 
-project = 'wliafe-mltools'
-copyright = '2025, wliafe'
-author = 'wliafe'
+project = "wliafe-mltools"
+copyright = "2025, wliafe"
+author = "wliafe"
 release = config["project"]["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'myst_parser',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "myst_parser",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'zh_CN'
+language = "zh_CN"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "classic"
-html_static_path = ['_static']
+html_static_path = ["_static"]
