c5d73e5cfbf0e6d9b84b6c6d072586d237d30bc7
wliafe <914099435@qq.com>
2025-11-06 09:13:44 +0800

    更新 docs 和 scripts 脚本

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -1,8 +1,8 @@
 import yaml
 import shutil
 import tomllib
-import subprocess
 from pathlib import Path
+from mltools import utils
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[2]  # 项目根目录
@@ -12,12 +12,14 @@ with open(ROOT / "pyproject.toml", "rb") as f:
 
 
 def set_readthedocs():
-    with open(ROOT / "docs/.readthedocs.yaml", "r", encoding="utf-8") as file:
-        data = yaml.safe_load(file)
+    """
+    设置 readthedocs 配置文件
+    """
     with open(ROOT / ".python-version", "r") as file:
         python_version = file.readline().strip()
-    data["build"]["tools"]["python"] = python_version
-    with open(ROOT / "docs/.readthedocs.yaml", "w", encoding="utf-8") as file:
+    with open(ROOT / "docs/.readthedocs.yaml", "r+", encoding="utf-8") as file:
+        data = yaml.safe_load(file)
+        data["build"]["tools"]["python"] = python_version
         yaml.dump(data, file)
 
 
@@ -33,21 +35,6 @@ def write_requirements():
             f.write(dep + "\n")
 
 
-def run_command(command):
-    process = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,  # 合并错误输出到标准输出
-        text=True,  # 返回字符串而非字节
-        bufsize=1,  # 行缓冲模式
-    )
-    while True:
-        line = process.stdout.readline()
-        if not line:
-            break
-        print(line.strip())
-
-
 def build_docs():
     """
     构建文档
@@ -60,5 +47,5 @@ def build_docs():
     doctrees_dir = Path("docs/build/doctrees")
     if doctrees_dir.exists():
         shutil.rmtree(doctrees_dir)
-    run_command(["uv", "sync"])
-    run_command(["uv", "run", "sphinx-build", "-M", "html", "docs/source/", "docs/build/"])
+    utils.bash_command(["uv", "sync"])
+    utils.bash_command(["uv", "run", "sphinx-build", "-M", "html", "docs/source/", "docs/build/"])
